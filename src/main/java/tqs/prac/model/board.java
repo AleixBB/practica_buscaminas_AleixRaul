9b4f89e568f5050be143dc0c5f41f5123296d7b3
Aleix Benet <aleixbenet@MacBook-Air-de-Aleix.local>
2025-10-06 20:44:04 +0200

    Testos constructor parametros

---
+++ src/main/java/tqs/prac/model/board.java
@@ -0,0 +1,20 @@
+package tqs.prac.model;
+import java.util.Random;
+
+public class board {
+    private int nMines;
+    private int size;
+    private char[][] matriuVisible;
+    private int[][] mines; //mines = -1
+
+
+    public board(int nMines, int size)
+    {
+        
+    }
+    public char getCellMatriuV(int fila, int col){}
+    public int getCellMines(int fila, int col){}
+
+
+}
+    
--- src/test/java/modelTest/modelTest.java
+++ src/test/java/modelTest/modelTest.java
@@ -1,23 +1,43 @@
 package modelTest;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*; 
-import tqs.prac.model.model;
+import tqs.prac.model.board;
 
 public class modelTest {
-    //test init
+    //test que comprova el constructor per defecte per la matriu visible (matriuVisible)
     @Test
-    void testInit()
+    void testInitpt1()
     {
     int mines = 4;
     int size = 6;
-    model tauler = new model(mines, size);
-    tauler.init();
+    board tauler = new board(mines, size);
     for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                assertEquals('-', tauler.getCell(i, j));
+                assertEquals('-', tauler.getCellMatriuV(i, j));
             }
         }
     }
+    //test init matriu int mines
+    @Test
+    void testInitpt2()
+    {
+        int mines = 3;
+        int size = 6;
+        board tauler = new board(mines, size);
+        for(int i=0; i<size; i++){
+            for (int j=0; j<size; j++)
+            {
+                assertEquals(0, tauler.getCellMines(i, j));
+            }
+        }
+
+    }
+
+
+
+
+
+
 
     
 }
