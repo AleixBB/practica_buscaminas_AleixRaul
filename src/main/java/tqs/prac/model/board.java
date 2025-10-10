c5ac77f80d47ee07ed5973b5fbe2bba137298ea1
Aleix Benet <aleixbenet@MacBook-Air-de-Aleix.local>
2025-10-10 21:24:57 +0200

    Adaptar classe Board segons tests de classe Board

--- src/main/java/tqs/prac/model/board.java
+++ src/main/java/tqs/prac/model/board.java
@@ -4,8 +4,7 @@ import java.util.Random;
 public class board {
     private int nMines;
     private int size;
-    private char[][] matriuVisible;
-    private int[][] mines; //mines = -1
+    private Cell[][] matrix;
 
 
     public board(int nMines, int size)
@@ -14,32 +13,21 @@ public class board {
         {
             throw new IllegalArgumentException("nombre de mines o mida fora de rang");
         }
-        
         this.nMines = nMines;
         this.size = size;
-        matriuVisible = new char[size][size];
-        mines = new int[size][size];
+        matrix = new Cell[size][size];
         for (int i=0; i<size; i++)
         {
             for (int j=0; j<size; j++)
             {
-                matriuVisible[i][j] = '-';
+                matrix[i][j] = new Cell(0);
             }
         }
-        for (int i=0; i<size; i++)
-        {
-            for (int j=0; j<size; j++)
-            {
-                mines[i][j] = 0;
-            }
-        }
+       
 
     }
-    public char getCellMatriuV(int fila, int col){
-        return matriuVisible[fila][col];
-    }
-    public int getCellMines(int fila, int col){
-        return mines[fila][col];
+    public Cell getCell(int fila, int col){
+        return matrix[fila][col];
     }
     public void putMinesintoBoard()
     {
@@ -48,8 +36,8 @@ public class board {
         while (count < nMines) {
             int r = rand.nextInt(size);
             int c = rand.nextInt(size);
-            if (mines[r][c] == 0) {
-                mines[r][c] = -1;
+            if (matrix[r][c].getValue() == 0) {
+                matrix[r][c].setValue(-1);
                 count++;
             }
         }   
