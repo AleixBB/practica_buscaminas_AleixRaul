b3bc572bbf1b3cc3fb6319733f5e726738f3bb33
Aleix Benet <aleixbenet@MacBook-Air-de-Aleix.local>
2025-11-22 12:34:39 +0100

    Primera versio testConstructor de game

--- src/main/java/tqs/prac/controller/Game.java
+++ src/main/java/tqs/prac/controller/Game.java
@@ -1,5 +1,10 @@
 package tqs.prac.controller;
 
 public class Game {
+    private Boolean gameOver;
+    private Boolean win;
+    private int flagsPlaced;
+    public Game(){
+    }
     
 }
--- src/test/java/controllerTest/GameTest.java
+++ src/test/java/controllerTest/GameTest.java
@@ -1,5 +1,19 @@
 package controllerTest;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*; 
+import tqs.prac.controller.Game;
+import tqs.prac.model.Cell;
+import tqs.prac.model.Board;
+import tqs.prac.Main;
+import org.junit.jupiter.api.Test;
 
 public class GameTest {
+    @Test
+    void testConstructor{
+        Game game = new Game();
+        assertFalse(game.getGameOver());
+        assertFalse(game.getWin());
+        assertEquals(0, game.getFlagsPlaced());
+    }
     
 }
