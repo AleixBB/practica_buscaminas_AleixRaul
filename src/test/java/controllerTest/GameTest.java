21f16686f05b80dc25dc2d2f70b0a687f35a8452
Aleix Benet <aleixbenet@MacBook-Air-de-Aleix.local>
2025-11-26 09:31:12 +0100

    Test pairwise testing a metode Act

--- src/test/java/controllerTest/GameTest.java
+++ src/test/java/controllerTest/GameTest.java
@@ -100,8 +100,44 @@ public class GameTest {
     AMB MINA, REVELADA, REVEAL*/
     @Test
     public void testActPairwise(){
-    //cas 1:
-    
+    MockGame game = new MockGame();
+    game.setUpMockGame(3);
+    //cas 1: BUIDA, OCULTA, REVEAL
+    game.act("REVEAL", 2, 1);
+    assertFalse(game.getWin());
+    assertFalse(game.getGameOver());
+    //cas 2: BUIDA, REVELADA, FLAG
+    try{
+        game.act("FLAG", 7,7);
+    }catch(Exception e){}
+    //cas 3: BUIDA, FLAGUEJADA, REVEAL
+    game.act("REVEAL", 7, 0);
+    assertFalse(game.getWin());
+    assertFalse(game.getGameOver());
+    //cas 4: NUMERADA, REVELADA, REVEAL
+    try{
+        game.act("REVEAL", 0,1);
+    }catch(Exception e) {}
+    //cas 5: NUMERADA, FLAGUEJADA, REVEAL
+    try{
+        game.act("REVEAL", 0,4);
+    }catch(Exception e){}
+    //cas 6: NUMERADA, OCULTA, FLAG
+    game.act("FLAG", 1,0);
+    assertFalse(game.getWin());
+    assertFalse(game.getGameOver());
+    //cas 7: AMB MINA, FLAGUEJADA, FLAG
+    game.act("FLAG", 4, 1);
+    assertFalse(game.getWin());
+    assertFalse(game.getGameOver());
+    //cas 8; AMB MINA, OCULTA, REVEAL
+    game.act("REVEAL", 0,0);
+    assertFalse(game.getWin());
+    assertTrue(game.getGameOver()); 
+    //cas 9: AMB MINA, REVELADA, REVEAL
+    try{
+        game.act("REVEAL", 0,0);
+    }catch(Exception e){}
 
 
     }
